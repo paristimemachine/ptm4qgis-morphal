538fb2b3ef5b1658a436e0ba84df7878b0e5ceb8
Eric Grosso <19222193+ericgrosso@users.noreply.github.com>
2024-01-17 13:29:05 +0000

    Improve formatting

--- morphal/core/morphal_segment_orientation.py
+++ morphal/core/morphal_segment_orientation.py
@@ -43,140 +43,134 @@ from .morphal_geometry_utils import *
 
 
 class MorphALSegmentOrientation(PTM4QgisAlgorithm):
-    INPUT = 'INPUT'
-    UNIT = 'UNIT'
-    INTERVAL = 'INTERVAL'
-    METHOD = 'CALC_METHOD'
-    ROUNDED = 'ROUNDED'
-    HISTOGRAM = 'HISTOGRAM'
-    HISTOGRAM_STEP = 'HISTOGRAM_STEP'
-    CLASSIFICATION = 'CLASSIFICATION'
-    CLASSIFICATION_STEP = 'CLASSIFICATION_STEP'
-    OUTPUT = 'OUTPUT'
+    INPUT = "INPUT"
+    UNIT = "UNIT"
+    INTERVAL = "INTERVAL"
+    METHOD = "CALC_METHOD"
+    ROUNDED = "ROUNDED"
+    HISTOGRAM = "HISTOGRAM"
+    HISTOGRAM_STEP = "HISTOGRAM_STEP"
+    CLASSIFICATION = "CLASSIFICATION"
+    CLASSIFICATION_STEP = "CLASSIFICATION_STEP"
+    OUTPUT = "OUTPUT"
 
     def help(self):
         # TODO improve help text
-        return self.tr('Compute the orientations of a layer of segments')
+        return self.tr("Compute the orientations of a layer of segments")
 
     def group(self):
-        return self.tr('MorphAL')
+        return self.tr("MorphAL")
 
     def groupId(self):
-        return 'morphal'
+        return "morphal"
 
     def __init__(self):
         super().__init__()
         self.export_z = False
         self.export_m = False
         self.distance_area = None
-        self.units = [self.tr('Degree'),
-                      self.tr('Radian'),
-                      self.tr('Grade')]
-        self.intervals = [self.tr('[0 ; Pi['),
-                          self.tr('[0 ; Pi/2[')]
+        self.units = [self.tr("Degree"), self.tr("Radian"), self.tr("Grade")]
+        self.intervals = [self.tr("[0 ; Pi["), self.tr("[0 ; Pi/2[")]
         # self.calc_methods = [self.tr('Layer CRS'),
         #                      self.tr('Project CRS'),
         #                      self.tr('Ellipsoidal')]
-        self.calc_methods = [self.tr('Layer CRS'),
-                             self.tr('Project CRS')]
-        self.errors = ''
+        self.calc_methods = [self.tr("Layer CRS"), self.tr("Project CRS")]
+        self.errors = ""
 
     def initAlgorithm(self, config):
         self.addParameter(
             QgsProcessingParameterFeatureSource(
-                self.INPUT,
-                self.tr('Input layer'),
-                types=[QgsProcessing.TypeVectorLine]
+                self.INPUT, self.tr("Input layer"), types=[QgsProcessing.TypeVectorLine]
             )
         )
 
         self.addParameter(
             QgsProcessingParameterEnum(
-                self.UNIT,
-                self.tr('Unit'),
-                options=self.units,
-                defaultValue=0
+                self.UNIT, self.tr("Unit"), options=self.units, defaultValue=0
             )
         )
 
         self.addParameter(
             QgsProcessingParameterEnum(
                 self.INTERVAL,
-                self.tr('Interval'),
+                self.tr("Interval"),
                 options=self.intervals,
-                defaultValue=0
+                defaultValue=0,
             )
         )
 
         self.addParameter(
             QgsProcessingParameterEnum(
                 self.METHOD,
-                self.tr('Calculate using'),
+                self.tr("Calculate using"),
                 options=self.calc_methods,
-                defaultValue=0
+                defaultValue=0,
             )
         )
 
         self.addParameter(
             QgsProcessingParameterBoolean(
                 self.ROUNDED,
-                self.tr('Rounded orientation result (3 digits)'),
-                defaultValue=True
+                self.tr("Rounded orientation result (3 digits)"),
+                defaultValue=True,
             )
         )
 
         self.addParameter(
             QgsProcessingParameterBoolean(
                 self.CLASSIFICATION,
-                self.tr('Compute a classification'),
-                defaultValue=True
+                self.tr("Compute a classification"),
+                defaultValue=True,
             )
         )
 
         self.addParameter(
             QgsProcessingParameterNumber(
                 self.CLASSIFICATION_STEP,
-                self.tr('Step of the classification'),
+                self.tr("Step of the classification"),
                 type=QgsProcessingParameterNumber.Double,
-                minValue=0.001, maxValue=200, defaultValue=10
+                minValue=0.001,
+                maxValue=200,
+                defaultValue=10,
             )
         )
 
         self.addParameter(
             QgsProcessingParameterBoolean(
-                self.HISTOGRAM,
-                self.tr('Create an histogram'),
-                defaultValue=False
+                self.HISTOGRAM, self.tr("Create an histogram"), defaultValue=False
             )
         )
 
         self.addParameter(
             QgsProcessingParameterNumber(
                 self.HISTOGRAM_STEP,
-                self.tr('Step of the histogram'),
+                self.tr("Step of the histogram"),
                 type=QgsProcessingParameterNumber.Double,
-                minValue=0.001, maxValue=200, defaultValue=5
+                minValue=0.001,
+                maxValue=200,
+                defaultValue=5,
             )
         )
 
         self.addParameter(
             QgsProcessingParameterFeatureSink(
-                self.OUTPUT,
-                self.tr('Segments with orientation')
+                self.OUTPUT, self.tr("Segments with orientation")
             )
         )
 
     def name(self):
-        return 'morphalsegmentorientation'
+        return "morphalsegmentorientation"
 
     def displayName(self):
         # TODO IMPROVE TEXT
-        return self.tr('Compute the orientations of a layer of segments')
+        return self.tr("Compute the orientations of a layer of segments")
 
     def processAlgorithm(self, parameters, context, feedback):
         source = self.parameterAsSource(parameters, self.INPUT, context)
         if source is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+            raise QgsProcessingException(
+                self.invalidSourceError(parameters, self.INPUT)
+            )
 
         # Unit:
         # 0 - degree
@@ -195,30 +189,37 @@ class MorphALSegmentOrientation(PTM4QgisAlgorithm):
 
         rounded = self.parameterAsBoolean(parameters, self.ROUNDED, context)
 
-        classification = self.parameterAsBoolean(parameters, self.CLASSIFICATION, context)
-        classification_step = self.parameterAsDouble(parameters, self.CLASSIFICATION_STEP, context)
+        classification = self.parameterAsBoolean(
+            parameters, self.CLASSIFICATION, context
+        )
+        classification_step = self.parameterAsDouble(
+            parameters, self.CLASSIFICATION_STEP, context
+        )
 
         histogram = self.parameterAsBoolean(parameters, self.HISTOGRAM, context)
-        histogram_step = self.parameterAsDouble(parameters, self.HISTOGRAM_STEP, context)
+        histogram_step = self.parameterAsDouble(
+            parameters, self.HISTOGRAM_STEP, context
+        )
 
         wkb_type = source.wkbType()
 
         # TODO ? improvement: test if the layer contains at least one segment
         if QgsWkbTypes.geometryType(wkb_type) != QgsWkbTypes.LineGeometry:
             # TODO IMPROVE FEEDBACK
-            feedback.reportError('The layer geometry type is different from a line')
+            feedback.reportError("The layer geometry type is different from a line")
             return {}
 
         fields = source.fields()
 
         new_fields = QgsFields()
-        new_fields.append(QgsField('orientation', QVariant.Double))
+        new_fields.append(QgsField("orientation", QVariant.Double))
         if classification:
-            new_fields.append(QgsField('classification', QVariant.Double))
+            new_fields.append(QgsField("classification", QVariant.Double))
 
         fields = QgsProcessingUtils.combineFields(fields, new_fields)
-        (sink, dest_id) = self.parameterAsSink(parameters, self.OUTPUT, context,
-                                               fields, wkb_type, source.sourceCrs())
+        (sink, dest_id) = self.parameterAsSink(
+            parameters, self.OUTPUT, context, fields, wkb_type, source.sourceCrs()
+        )
         if sink is None:
             raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
 
@@ -230,8 +231,12 @@ class MorphALSegmentOrientation(PTM4QgisAlgorithm):
         #     self.distance_area.setEllipsoid(context.ellipsoid())
         if method == 1:
             if not context.project():
-                raise QgsProcessingException(self.tr('No project is available in this context'))
-            coord_transform = QgsCoordinateTransform(source.sourceCrs(), context.project().crs(), context.project())
+                raise QgsProcessingException(
+                    self.tr("No project is available in this context")
+                )
+            coord_transform = QgsCoordinateTransform(
+                source.sourceCrs(), context.project().crs(), context.project()
+            )
 
         features = source.getFeatures()
         total = 100.0 / source.featureCount() if source.featureCount() else 0
