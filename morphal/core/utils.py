c9d54454a2e17e456332a460193583d4fd9eff50
Eric Grosso <19222193+ericgrosso@users.noreply.github.com>
2024-07-12 10:18:06 +0200

    Add utils: LayerRenamer

---
+++ morphal/core/utils.py
@@ -0,0 +1,11 @@
+from qgis.core import QgsProcessingLayerPostProcessorInterface
+
+
+class LayerRenamer(QgsProcessingLayerPostProcessorInterface):
+
+    def __init__(self, layer_name):
+        self.name = layer_name
+        super().__init__()
+
+    def postProcessLayer(self, layer, context, feedback):
+        layer.setName(self.name)
